659141267c8e3a8628ef5565b5293daf5387f782
GabeCordo <gabe.cordovado@yahoo.ca>
2022-03-14 20:51:33 -0400

    create user UI and ERROR hints
    Added the HTML form inputs for User creation, this includes adding error hints when invalid data has been passed through to fields such as SSN, email, dob, and age.

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+from venv import create
 from flask import (
     Flask, 
     request, 
@@ -15,7 +16,10 @@ from flask_login import (
 from flask_login.utils import login_required
 from flask_wtf import CSRFProtect
 
-from db import authenticate_user, fetch_user
+from db import authenticate_user, fetch_user, create_user
+
+from datetime import datetime
+import re
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
@@ -68,10 +72,101 @@ def logout():
 def dashboard():
     return render_template("dashboard.html")
 
-@app.route('/admin/createuser')
+@app.route('/admin/createuser', methods=["GET", "POST"])
 @login_required
-def createuser():
-    return render_template("createuser.html")
+def create_user_page():
+    if request.method == "POST":
+        
+        invalid_name = False  # will be marked as true if anything has been submitted empty
+        first_name = request.form.get("first-name")
+        middle_name = request.form.get("middle-name")
+        last_name = request.form.get("last-name")
+        # ensure none of the first/middle/last name entries are blank
+        if (first_name == "") or (middle_name == "") or (last_name == ""):
+            invalid_name = True
+            
+        invalid_age = False
+        gender = int(request.form.get("gender"))
+        # ensure that the geneder is a (0) male, (1) female, or (2) other
+        if (gender < 0) or (gender > 2):
+            invalid_age = True
+        # the date of birth is sent via HTTP in the format YYYY-MM-DD, we need to convert to datetime
+        date_of_birth = datetime.strptime(request.form.get("date-birth"), "%Y-%m-%d")
+        # age can be sent as '', so if that is being sent to use attempting to convert this to an int
+        # right away will create a str->int type conversion error (validate this first)
+        if (request.form.get("age") != ""):
+            age = int(request.form.get("age"))
+            # we want to perform integer devision of the days elapsed (by 365) to determine whether the
+            # number of elapsed years since present matches the age given/entered by/for the client
+            years_difference = (datetime.now() - date_of_birth).days // 365
+            # also ensure that the client is NOT under 13 years old
+            if (age < 13) or (age != years_difference):
+                invalid_age = True
+        else:
+            invalid_age = True
+        
+        invalid_address = False  # will be marked as true if anything has been submitted empty
+        address = request.form.get("address")
+        street_name = request.form.get("street-name")
+        # verify that the address and street-name are not empty strings
+        if (address != "") or (street_name == ""):
+            invalid_address = True
+        # house # can be sent as '', so if that is being sent to use attempting to convert this to an int
+        # right away will create a str->int type conversion error (validate this first)
+        if (request.form.get("house-number") != ""):
+            house_number = int(request.form.get("house-number"))
+            # ensure that we have not entered a negative house number (I don't think anyone has this)
+            if house_number < 0:
+                invalid_address = True
+        else:
+            invalid_address = True
+            
+        invalid_password = False
+        password = request.form.get("password")
+        # just check to make sure the password isn't empty and that it's at least 4 characters long
+        # otherwise this can be considered a "weak" password choice
+        if (password != "") or (len(password) < 4):
+            invalid_password = True
+        
+        invalid_ssn = False
+        # first check that the ssn is valid otherwise the int->str type conversion will fail
+        if (request.form.get("ssn") != ""):
+            ssn = int(request.form.get("ssn"))  # this will be used when we try and submit it to postgres
+            # an SSN must be 9 characters long according to the Canadian government standard
+            if len(request.form.get("ssn")) != 9:
+                invalid_ssn = True
+        else:
+            invalid_ssn = True
+            
+        invalid_phone = False
+        phone_number = request.form.get("phone-number")
+        # regex found @ https://stackoverflow.com/questions/5294314/python-get-number-of-years-that-have-passed-from-date-string
+        if not re.match("(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})", phone_number):
+            invalid_phone = True
+        
+        # ensure that we have not generate a single error, if we have, update the HTML with the appropriate error hint
+        if (not invalid_name) or (not invalid_address) or (not invalid_age) or (not invalid_password) or (not invalid_phone) or (not invalid_ssn):
+            return render_template(
+                "createuser.html",
+                invalid_name=invalid_name,
+                invalid_address=invalid_address,
+                invalid_age=invalid_age,
+                invalid_password=invalid_password,
+                invalid_phone=invalid_phone,
+                invalid_ssn=invalid_ssn
+            )
+        else:
+            # give all the data in the form is valid, submit it to postgres
+            
+            # TODO - submit a user to the postgres db
+            
+            # no error has been generated, display that the user creation was successful
+            return render_template(
+                "createuser.html", 
+                success=True
+            )
+    else:
+        return render_template("createuser.html")
 
 if __name__ == "__main__":
     app.run()
